91906a9d93dfa1d6996402e033c43f413ba8b315
pgs787 <pgs787@naver.com>
2020-02-06 13:49:27 +0900

    enemyrains 완료

--- js/enemy.js
+++ js/enemy.js
@@ -1,56 +1,109 @@
-const parent = document.querySelector("#bg");
+const backGround = document.querySelector("#bg");
 const heroPostion = document.querySelector(".hero");
 const heroStyle = window.getComputedStyle(heroPostion);
+const startBtn = document.querySelector(".start-btn");
+const stopBtn = document.querySelector(".stop-btn");
+const bgF = document.querySelector(".bg-font");
 const ghostSound = new Audio("audio.wav"); // 고스트 죽음 사운드
 
+const gg = document.querySelector(".ghost");
+const life = document.querySelector(".life");
+const point = document.querySelector(".point");
+
+let pointCount = 0;
 // 고스트 생성
-function ghostCre() {
+function ghostCre(ghostMake) {
   const ghost = document.createElement("div");
   const style = window.getComputedStyle(ghost);
   const random = Math.floor(Math.random() * 760);
 
   ghost.className = "ghost";
   ghost.style.left = String(random) + "px";
-  parent.appendChild(ghost);
+  backGround.appendChild(ghost);
 
+  // stop 버튼 누르면 고스트 삭제
+  stopBtn.addEventListener("click", function() {
+    ghost.remove();
+  });
   // 고스트 아래로 이동
-  setInterval(function() {
+  const downM = setInterval(function() {
     const topValue = parseInt(style.top);
     ghost.style.top = topValue + 1 + "px";
 
     // 고스트 삭제
     // 고스트 영웅에 겹칠 때 삭제
     if (
-      ghost.offsetTop > 506 &&
+      ghost.offsetTop === 506 &&
       ghost.offsetLeft + 35 > heroPostion.offsetLeft &&
       ghost.offsetLeft - 35 < heroPostion.offsetLeft
     ) {
+      pointCount += 10;
+      point.innerText = pointCount;
+      // 고스트 사운드
       ghostSound.play();
       ghost.className = "ghost-dead";
 
       setTimeout(function() {
         ghost.remove();
-      }, 500);
+      }, 50);
     }
 
-    // 고스트 바닥에 닿을 때 삭제
-    if (ghost.offsetTop > 550) {
-      ghost.style.top = 550 + "px";
+    // 고스트 바닥에 닿을 때 삭제 및 Game Over
+    if (ghost.offsetTop === 555) {
+      life.removeChild(life.firstChild);
+      ghost.style.top = 545 + "px";
       ghost.className = "ghost-dead";
-
       setTimeout(function() {
         ghost.remove();
-      }, 300);
+      }, 50);
+    }
+    if (life.childElementCount === 0) {
+      clearInterval(downM);
+      clearInterval(ghostMake);
+      const div = document.createElement("div");
+      div.className = "gameover";
+      div.innerHTML = "Game over";
+      backGround.appendChild(div);
+      backGround.style.background = "black";
+      backGround.style.opacity = "0.7";
     }
   }, 2);
 }
 
-function init() {
-  // 고스트생성주기
-
-  setInterval(function() {
-    const newGhost = new ghostCre();
-  }, 3000); //3200
+// 게임 스타트, 중지 글씨
+function startFont() {
+  bgF.style.display = "block";
+  bgF.className = "bg-start";
+  bgF.innerHTML = "start";
+  setTimeout(function() {
+    bgF.style.display = "none";
+  }, 680);
 }
 
-init();
+function stopFont() {
+  bgF.style.display = "block";
+  bgF.className = "bg-stop";
+  bgF.innerHTML = "stop";
+  setTimeout(function() {
+    bgF.style.display = "none";
+  }, 680);
+}
+
+// 고스트 생성
+function gameStartandStop() {
+  const ghostMake = setInterval(function() {
+    const newGhost = new ghostCre(ghostMake);
+  }, 1500);
+
+  // stop 버튼 누르면 고스트 생성 중지
+  stopBtn.addEventListener("click", function() {
+    clearInterval(ghostMake);
+  });
+  stopBtn.addEventListener("click", function() {
+    point.innerHTML = 0;
+  });
+}
+
+startBtn.addEventListener("click", gameStartandStop);
+startBtn.addEventListener("click", startFont);
+stopBtn.addEventListener("click", stopFont);
--- js/hero.js
+++ js/hero.js
@@ -4,15 +4,15 @@ const style = window.getComputedStyle(hero);
 const Move = e => {
   const leftValue = parseInt(style.left);
   // 왼쪽
-  if (e.keyCode === 37 && hero.style.left !== "0px") {
+  if (e.keyCode === 37 && hero.style.left !== "4px") {
     hero.className = "move-left";
-    hero.style.left = leftValue - 8 + "px";
+    hero.style.left = leftValue - 12 + "px";
   }
   // 오른쪽
-  if (e.keyCode === 39 && hero.style.left !== "764px") {
+  if (e.keyCode === 39 && hero.style.left !== "760px") {
     let leftValue = parseInt(style.left);
     hero.className = "move-right";
-    hero.style.left = leftValue + 8 + "px";
+    hero.style.left = leftValue + 12 + "px";
   }
   window.addEventListener("keyup", function() {
     hero.className = "hero";
